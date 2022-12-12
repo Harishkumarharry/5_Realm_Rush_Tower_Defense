ec9ab7ca8ec6ae6b6d98950f472bbde911e48f2e
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2022-12-11 19:05:22 -0700

    Introduced GUI
    Completed bank system and added GUI

--- 5_Realm_Rush_Tower_Defense/Assets/Bank/Bank.cs
+++ 5_Realm_Rush_Tower_Defense/Assets/Bank/Bank.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -9,17 +11,39 @@ public class Bank : MonoBehaviour
 
     public int CurrentBalance { get {return currentBalance; } }
 
+    [SerializeField] TextMeshProUGUI displayBalanace;
+
     void Awake()
     {
         currentBalance = startingBalance;
+        updateDisplay();
     }
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        updateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        updateDisplay();
+
+        if(currentBalance < 0)
+        {
+            //Lose the game;
+            ReloadScene();
+        }
+    }
+
+    void updateDisplay()
+    {
+        displayBalanace.text = "Gold: " + currentBalance;
+    }
+
+    void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
